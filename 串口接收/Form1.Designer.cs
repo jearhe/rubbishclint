5c9a13b1f5167887ac45112b8cc8f9f5d9e9adb5
overme <overme@DESKTOP-J4TVLRQ>
2018-05-31 21:25:21 +0800

    初始化项目

---
+++ 串口接收/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace 串口接收
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtShowData = new System.Windows.Forms.RichTextBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.bt_clear2 = new System.Windows.Forms.Button();
+            this.bt_lianjie = new System.Windows.Forms.Button();
+            this.RTB_client = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(196, 31);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(61, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开串口";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtShowData
+            // 
+            this.txtShowData.Location = new System.Drawing.Point(6, 119);
+            this.txtShowData.Name = "txtShowData";
+            this.txtShowData.Size = new System.Drawing.Size(295, 315);
+            this.txtShowData.TabIndex = 1;
+            this.txtShowData.Text = "";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(196, 71);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(57, 23);
+            this.btn_clear.TabIndex = 9;
+            this.btn_clear.Text = "清空";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.btn_clear);
+            this.groupBox1.Controls.Add(this.txtShowData);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 445);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口读取";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7"});
+            this.comboBox1.Location = new System.Drawing.Point(75, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(77, 20);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "串口";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.bt_clear2);
+            this.groupBox2.Controls.Add(this.bt_lianjie);
+            this.groupBox2.Controls.Add(this.RTB_client);
+            this.groupBox2.Location = new System.Drawing.Point(340, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 438);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "发送服务器";
+            // 
+            // bt_clear2
+            // 
+            this.bt_clear2.Location = new System.Drawing.Point(95, 64);
+            this.bt_clear2.Name = "bt_clear2";
+            this.bt_clear2.Size = new System.Drawing.Size(90, 23);
+            this.bt_clear2.TabIndex = 6;
+            this.bt_clear2.Text = "清空";
+            this.bt_clear2.UseVisualStyleBackColor = true;
+            this.bt_clear2.Click += new System.EventHandler(this.bt_clear2_Click);
+            // 
+            // bt_lianjie
+            // 
+            this.bt_lianjie.Location = new System.Drawing.Point(95, 24);
+            this.bt_lianjie.Name = "bt_lianjie";
+            this.bt_lianjie.Size = new System.Drawing.Size(90, 23);
+            this.bt_lianjie.TabIndex = 5;
+            this.bt_lianjie.Text = "建立连接";
+            this.bt_lianjie.UseVisualStyleBackColor = true;
+            this.bt_lianjie.Click += new System.EventHandler(this.bt_lianjie_Click);
+            // 
+            // RTB_client
+            // 
+            this.RTB_client.Location = new System.Drawing.Point(8, 112);
+            this.RTB_client.Name = "RTB_client";
+            this.RTB_client.Size = new System.Drawing.Size(273, 315);
+            this.RTB_client.TabIndex = 2;
+            this.RTB_client.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 464);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "智能垃圾桶系统";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.RichTextBox txtShowData;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button bt_lianjie;
+        private System.Windows.Forms.RichTextBox RTB_client;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button bt_clear2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ 串口接收/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace 串口接收
+{
+    public partial class Form1 : Form
+    {
+        int k = 0;
+        string str;
+
+        //添加接收事件开关
+        bool isfrist = true;
+        //垃圾桶高度
+        public int h1=0, h2 = 0, h3 = 0, h4 = 0;
+        //下载当前数据
+        WebClient wc = new WebClient();
+        //开关
+        bool isconnet = false;
+        //实例化一个SerialPort
+        private SerialPort ComDevice = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //打开串口
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (ComDevice.IsOpen == false)
+            {
+                if (comboBox1.SelectedIndex==-1)
+                {
+                    MessageBox.Show("输入串口号");
+                }
+                else { 
+                    //串口号
+                    ComDevice.PortName = comboBox1.Text;
+                    //波特率
+                    ComDevice.BaudRate = Convert.ToInt32("115200");
+                    //设置奇偶校验检查协议
+                    ComDevice.Parity = Parity.None;
+                    //每个字节的标准数据位长度
+                    ComDevice.DataBits = 8;
+                    //设置每个字节的标准停止位数
+                    ComDevice.StopBits = StopBits.One;
+                    try
+                    {
+                        ComDevice.Open();
+                        //绑定事件
+                        if (isfrist)
+                            ComDevice.DataReceived += new SerialDataReceivedEventHandler(Com_DataReceived);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    btnOpen.Text = "关闭串口";
+                }
+            }
+            else
+            {
+                try
+                {
+                    ComDevice.Close();
+                    isfrist = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                btnOpen.Text = "打开串口";
+            }
+        }
+        /// <summary>
+        /// 串口接收数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            str += ComDevice.ReadExisting();//字符串方式读
+            if (str.Length >= 20)
+            {
+                kk(str);
+                str = "";
+            }
+        }
+
+
+        /// <summary>
+        /// 分析数据中的四个高度
+        /// </summary>
+        /// <param name="str">接收的字符串</param>
+        void kk(string str)
+        {
+            h1=int.Parse(str.Substring(1,2));
+            h2 = int.Parse(str.Substring(6, 2));
+            h3 = int.Parse(str.Substring(11, 2));
+            h4 = int.Parse(str.Substring(16, 2));
+            detail(h1,h2,h3,h4);
+            txtShowData.AppendText("h1:"+h1.ToString()+ "h2:"+ h2.ToString()+ "h3:"+ h3.ToString()+ "h4:"+ h4.ToString()+"\n");
+        }
+
+        /// <summary>
+        /// 向服务器提交参数
+        /// </summary>
+        /// <param name="a">h1</param>
+        /// <param name="b">h2</param>
+        /// <param name="c">h3</param>
+        /// <param name="d">h4</param>
+        void detail(int a,int b,int c,int d)
+        {
+            if (isconnet)
+            {
+                str = wc.DownloadString("http://www.overme.xin/Rubbish/postRubbish?h1=" + a + "&h2=" + b + "&h3=" + c + "&h4=" + d + "");
+                if (str == "\"1\"")
+                {
+                    RTB_client.Text +="("+a+" "+b + " " + c + " " + d + ")" + "存入数据库成功\n";
+                }
+                else
+                {
+                    RTB_client.Text += "存入数据库失败\n";
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 清空按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bt_clear2_Click(object sender, EventArgs e)
+        {
+            RTB_client.Text = "";
+        }
+
+        /// <summary>
+        /// 随机生成高度，测试用
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            random.Next(0, 24);
+
+            detail(random.Next(0, 20), random.Next(0, 20), random.Next(0, 20), random.Next(0, 20));
+        }
+
+
+        /// <summary>
+        /// 清空按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txtShowData.Text = "";
+        }
+
+        /// <summary>
+        /// 连接按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bt_lianjie_Click(object sender, EventArgs e)
+        {
+            if (isconnet)
+            {
+                isconnet = !isconnet;
+                bt_lianjie.Text = "建立连接";
+            }
+            else
+            {
+                isconnet = !isconnet;
+                bt_lianjie.Text = "断开连接";
+            }
+               
+
+        }
+
+
+    }
+}
